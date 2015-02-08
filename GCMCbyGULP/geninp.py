d16130e6ac3d5c9e65e23988719d9c5d322fe9f5
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2015-02-08 00:49:06 -0600

    Preliminarily finish the main functions
    The two main functions has been preliminarily finished, which are both
    basically skelontons.

--- GCMCbyGULP/geninp.py
+++ GCMCbyGULP/geninp.py
@@ -3,3 +3,28 @@ The input generator
 ===================
 
 """
+
+
+import argparse
+
+from .simultask import gen_simul_task_from_file
+
+
+def gen_main():
+    """The main function for the input generator
+    """
+
+    # Parse the command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', metavar='INP', nargs=1,
+                        type=argparse.FileType('r'),
+                        help='The name of the JSON/YAML input file')
+    args = parser.parse_args()
+
+    # Generate the simulation task object from the input file.
+    simul_task = gen_simul_task_from_file(args.input)
+
+    # Generate the input files for the simulation tasks.
+    simul_task.gen_inp()
+
+    return 0
--- GCMCbyGULP/readres.py
+++ GCMCbyGULP/readres.py
@@ -3,3 +3,54 @@ Result reader for GCMC simulations
 ==================================
 
 """
+
+
+import argparse
+import os.path
+
+from simultask import gen_simul_task_from_file
+
+
+def read_main():
+    """The main function for the read script"""
+
+    # Parse the command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', metavar='INP', nargs=1,
+                        type=argparse.FileType('r'),
+                        help='The name of the JSON/YAML input file'
+                        )
+    parser.add_argument('-o', '--out', metavar='FILE', nargs=1,
+                        type=argparse.FileType('w'),
+                        default=argparse.SUPPRESS,
+                        help='The output file for dumping the result'
+                        )
+    parser.add_argument('-f', '--format', metavar='FORMAT', nargs=1,
+                        type=str, choices=[
+                            'txt', 'mat',
+                            ],
+                        default='txt'
+                        )
+    args = parser.parse_args()
+
+    # Attempt to open the output file
+    if 'out' in args:
+        out_file = args.out
+    else:
+        out_file_name = args.input.name.split('.')[0] + '.out',
+        if os.path.isfile(out_file_name):
+            raise ValueError(
+                'The output file {} already exists.'.format(out_file_name) +
+                '\nTo overwrite, please use the -o option.'
+                )
+        else:
+            out_file = open(out_file_name, 'w')
+
+    # Generate the simulation task object
+    simul_task = gen_simul_task_from_file(args.input)
+
+    # Gather the results and output to a file
+    simul_task.gather_res()
+    simul_task.dump_res(args.format, out_file)
+
+    return 0
