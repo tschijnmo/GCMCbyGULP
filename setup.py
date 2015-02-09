562459bf23bf29c489fe1db5882540c5ad3eec05
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2015-02-09 11:55:05 -0600

    Add the setup script
    The setup script has been finished with the setuptools. The scripts are
    added by using the entry point rather than hard-coded scripts.

--- setup.py
+++ setup.py
@@ -3,3 +3,31 @@ Setup script for GCMCbyGULP
 ===========================
 
 """
+
+from setuptools import setup, find_packages
+
+setup(
+    name='GCMCbyGULP',
+    version='0.1.0',
+    description='Utilities for performing GCMC simulations by GULP',
+    author='Tschijnmo TSCHAU',
+    author_email='tschijnmotschau@gmail.com',
+    url='https://github.com/tschijnmo/GCMCbyGULP',
+    license='MIT',
+
+    packages=find_packages(),
+    install_requires=[
+        'CoolProp',
+        'periodic',
+        'pystache',
+        'numpy',
+        'scipy',
+        'PyYAML',
+        ],
+    entry_points={
+        'console_scripts': [
+            'GCMCbyGULP-gen = GCMCbyGULP.geninp:gen_main',
+            'GCMCbyGULP-read = GCMCbyGULP.readres:read_main',
+            ],
+        },
+    )
